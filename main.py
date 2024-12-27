c6605b94229b19f0e40befa1061af4e8a52c91d2
PromethianKnight <ashishsharma12112@gmail.com>
2024-12-27 20:27:08 +0530

    This is the 1st of many

---
+++ main.py
@@ -0,0 +1,9 @@
+# 1st program to print my name
+
+# hello_world function to accept 1 argument and print two statements
+def hello_world(name):  # It is a good practice to have function names in lowercase according to PyCharm
+    print(f'Hi, {name}')
+    print('This is my 1st python program')
+
+if __name__ == '__main__':
+    hello_world('Github')
