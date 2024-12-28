b5f870d16622f48add83795021b8b0156667b503
PromethianKnight <ashishsharma12112@gmail.com>
2024-12-28 23:22:51 +0530

    String operations

---
+++ String methods/Testing out strings.py
@@ -0,0 +1,5 @@
+description = 'This is a string, they can be big or small, filled with any range of characters and as long as they are between the single or double quotes, they can even include keywords'
+
+# Different operations possible
+print(description)  # Print the entire string
+print(len(description)) # Print the length of the string
