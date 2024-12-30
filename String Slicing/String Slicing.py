4b3eee0664d9a836d4d9ae15ea3678cc4fe34d75
PromethianKnight <ashishsharma12112@gmail.com>
2024-12-30 21:26:57 +0530

    Slicing strings up!

--- String Slicing/String Slicing.py
+++ String Slicing/String Slicing.py
@@ -6,3 +6,6 @@ sentence = 'This is a sentence with words 1234567890 !@#$%^&*()_+'
 
 print(sentence[0:21])   # Print the substring from index 0 till 21 but not including the 21's index
 
+print(sentence[:21])   # Another way of writing it
+
+print(sentence[40:])   # Similarly
