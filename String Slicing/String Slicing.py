73418eeaa57daf61362a92dbdf08cd703d5ffd93
PromethianKnight <ashishsharma12112@gmail.com>
2024-12-30 21:20:50 +0530

    Slicing strings up!

---
+++ String Slicing/String Slicing.py
@@ -0,0 +1,8 @@
+# WAP to demonstrate various string slicing use cases
+
+# Slicing: creates a substring by extracting elements from another string [start:stop:step]
+
+sentence = 'This is a sentence with words 1234567890 !@#$%^&*()_+'
+
+print(sentence[0:21])   # Print the substring from index 0 till 21 but not including the 21's index
+
